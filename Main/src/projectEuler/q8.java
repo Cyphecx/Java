16fed58ddc8faebc18850fdbc627e663824047ee
Home <Home@Desktop.home>
2016-08-11 19:18:35 -0400

    raw

--- Main/src/projectEuler/q8.java
+++ Main/src/projectEuler/q8.java
@@ -13,7 +13,6 @@ import java.io.IOException;
 //
 //Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
 
-
 public class q8 {
 
 	public static void main(String[] args) throws IOException {
