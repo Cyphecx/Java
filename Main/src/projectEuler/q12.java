50fc1553de22f4f02245351c8dd344788fe7ba4d
Wyatt Potato <whauben11@gmail.com>
2017-03-13 15:54:48 -0400

    meh

--- Main/src/projectEuler/q12.java
+++ Main/src/projectEuler/q12.java
@@ -1,8 +1,19 @@
 package projectEuler;
 
+import java.util.ArrayList;
+
 public class q12{
 	public static void main(String[] args){
-		
+		int div = 0;
+		long ctr = 2;
+		while(div < 500){
+			div = 0;
+			ArrayList list = utils.primeFactor.factor((ctr * (ctr + 1)) / 2);
+			for(int i = 0; i < list.size(); i++){
+				System.out.println(list.get(i));
+			}
+			ctr++;
+		}
 	}
 	
 }
--- Main/src/utils/primeFactor.java
+++ Main/src/utils/primeFactor.java
@@ -5,25 +5,48 @@ import java.util.ArrayList;
 public class primeFactor {
 	static ArrayList ans = new ArrayList();
 	public static ArrayList factor(long n){
-		for(long i = 2; i < Math.sqrt(n); n++){
+		long i = 2;
+		long num = 2;
+		while(n % num != 0){
 			if(n % i == 0){
-				if(checkPrime.isPrime2(i)){
-					ans.add(i);
-				}
-				else{
-					factor2(i);
-				}
-				if(checkPrime.isPrime2(n / (n/i))){
-					ans.add(n/i);
-				}
-				else{
-					factor2(n/i);
-				}
+				num = i;
 			}
+			i++;
 		}
-		return null;
+		if(checkPrime.isPrime2(num)){
+			ans.add(num);
+		}
+		else{
+			factor2(num);
+		}
+		if(checkPrime.isPrime2((n/num))){
+			ans.add(n/num);
+		}
+		else{
+			factor2(n/num);
+		}
+		return ans;
 	}
 	public static void factor2(long n){
-		
+		long i = 2;
+		long num = 2;
+		while(n % num != 0){
+			if(n % i == 0){
+				num = i;
+			}
+			i++;
+		}
+		if(checkPrime.isPrime2(num)){
+			ans.add(num);
+		}
+		else{
+			factor2(num);
+		}
+		if(checkPrime.isPrime2(n/num)){
+			ans.add(n/num);
+		}
+		else{
+			factor2(n/num);
+		}
 	}
 }
