fa88fe12449976521a2c134f9c418c24846ccb54
coditumlabs <coditumlabs@10.144.4.31>
2017-03-13 17:56:02 -0400

    working on shell-based matrix input

--- Main/src/coditum/matrix/Matrix.java
+++ Main/src/coditum/matrix/Matrix.java
@@ -1,8 +1,12 @@
 package coditum.matrix;
 
+import java.util.Scanner;
+
 public class Matrix {
 	private float m;
 	private float n;
+	private int m1;
+	private int n1;
 	float[][] data;
 
 	public Matrix(int m, int n){
@@ -50,8 +54,6 @@ public class Matrix {
 					for(int z = 0; z < data[i].length; z++){
 						data[currR][z] = temp[z];
 					}
-					System.out.println("meow");
-					printMatrix();
 					for(int z = 0; z < data.length; z++){
 						if(z != currR){
 							rowSub(z,currR,data[z][currR] / data[currR][currR]);
@@ -63,7 +65,6 @@ public class Matrix {
 					break;
 				}
 			}
-			printMatrix();
 			currR++;
 		}
 	}
@@ -79,17 +80,28 @@ public class Matrix {
 	public void printMatrix(){
 		for(int i = 0; i < data.length; i++){
 			for(int z = 0; z < data[i].length; z++){
-				if(data[i][z] % 1 == 0){
-					System.out.print((int)data[i][z] + ",");
+				if(z == data[i].length-1){
+					if(data[i][z] % 1 == 0){
+						System.out.print((int)data[i][z]);
+					}
+					else{
+						System.out.print(data[i][z]);
+					}
 				}
 				else{
-					System.out.print(data[i][z]+",");
+					if(data[i][z] % 1 == 0){
+						System.out.print((int)data[i][z] + ",");
+					}
+					else{
+						System.out.print(data[i][z]+",");
+					}
 				}
 			}
 			System.out.println("");
 		}
 		System.out.println("");
 	}
+	
 	public Matrix matrixMulti(Matrix inMa1, Matrix inMa2) throws MatrixSizeMismatch{
 		Matrix matrix = new Matrix(inMa1.height(), inMa2.width());
 		for(int i = 0; i < inMa2.height(); i++){
@@ -106,5 +118,27 @@ public class Matrix {
 		}
 		//return this;
 		return matrix;
+	}
+	public void getInput(){
+		m1 = 0;
+		n1 = 0;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please enter an array in the format [data, data, data... ;(New Line) data, data, data...] for an array size: " + n + "x" + m);
+		String input = scan.nextLine();
+		for(int i = 0; i < input.length(); i++){
+			if(input.charAt(i) == '['){
+				int end = i;
+				while(input.charAt(end) != ','){
+					end++;
+				}
+				
+			}
+		
+		}
+	}
+	public void iterator(){
+		if(n1 == this.n1){
+			
+		}
 	}
 }
--- Main/src/coditum/matrix/matrixTester.java
+++ Main/src/coditum/matrix/matrixTester.java
@@ -3,7 +3,7 @@ public class matrixTester{
 	public static void main(String[] args) throws MatrixSizeMismatch{
 		Matrix m1 = new Matrix(3,3);
 		m1.set(0, 0, 1);
-		m1.set(0, 1, 2);
+		m1.set(0, 1, 0);
 		m1.set(0, 2, 0);
 		m1.set(1, 0, 0);
 		m1.set(1, 1, 1);
@@ -11,16 +11,14 @@ public class matrixTester{
 		m1.set(2, 0, 0);
 		m1.set(2, 1, 0);
 		m1.set(2, 2, 1);
+		m1.getInput();
 		Matrix m2 = new Matrix(3,3);
-		m2.set(0, 0, 5);
-		m2.set(0, 1, 6);
-		m2.set(0, 2, 2);
-		m2.set(1, 0, 4);
-		m2.set(1, 1, 6);
-		m2.set(1, 2, 1);
-		m2.set(2, 0, 7);
-		m2.set(2, 1, 7);
-		m2.set(2, 2, 7);
+		for(int i = 0; i < 3; i++){
+			for(int z = 0; z < 3; z++){
+				m2.set(i, z, (int)(Math.random()*100+1));
+			}
+		}
+		m2.printMatrix();
 		Matrix ans = m1.matrixMulti(m1, m2);
 		ans.printMatrix();
 	}
