0341b68241d15873f613a54e10a7e280689e23f0
Wyatt H. <whauben11@gmail.com>
2017-06-09 14:46:12 -0400

    ed

--- Main/src/test.java
+++ Main/src/test.java
@@ -5,7 +5,7 @@ import java.util.Set;
 public class test{
 
 	public static void main(String[] args) {
-		System.out.println(utils.primeFactor.factor());
+		System.out.println(utils.primeFactor.factor(0));
 	}
 
 }
