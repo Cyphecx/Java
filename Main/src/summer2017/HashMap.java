b27e9a8d583478576b0230778c14594fd25add6a
Wyatt Potato <whauben11@gmail.com>
2017-06-25 20:19:11 -0400

    Started Hashmap/renamed head folder

---
+++ Main/src/summer2017/HashMap.java
@@ -0,0 +1,91 @@
+package summer2017;
+
+import java.security.MessageDigest;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+public class HashMap<T, K> implements Map<T,K>{
+	1
+	public HashMap(){
+		
+	}
+	public HashMap(int sSize){
+		
+	}
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean containsKey(Object arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsValue(Object arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Set entrySet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public K get(Object arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Set keySet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object put(Object arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void putAll(Map arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public K remove(Object arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Collection values() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public int Hash(T key){
+		return key.hashCode();
+	}
+}
---
+++ Main/src/summer2017/Node.java
@@ -0,0 +1,10 @@
+package summer2017;
+
+public class Node<T,K>{
+	T key;
+	K value;
+	public Node(T key,K value){
+		this.key = key;
+		this.value = value;
+	}
+}
